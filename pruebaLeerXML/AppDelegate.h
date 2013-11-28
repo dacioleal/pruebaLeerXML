41e6d0ed8d6dd3d239cdbd017741f82ad0c3ea1e
dacioleal <dacioleal@gmail.com>
2013-11-28 12:06:01 +0100

    Initial Commit

---
+++ pruebaLeerXML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pruebaLeerXML
+//
+//  Created by Dacio Leal Rodriguez on 28/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pruebaLeerXML/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pruebaLeerXML
+//
+//  Created by Dacio Leal Rodriguez on 28/11/13.
+//  Copyright (c) 2013 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
